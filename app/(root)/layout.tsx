80dd1c9928c4b46ae277ddee5cf117f9234cf970
Aayushjain1407 <aayushjain1475@gmail.com>
2025-01-13 19:08:53 +0530

    Implemented RightSideBar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,9 +2,7 @@ import { ReactNode } from "react";
 
 import Navbar from "@/components/navigation/navbar";
 import LeftSideBar from "@/components/navigation/LeftSidebar";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
-import { Link } from "lucide-react";
+import RightSidebar from "@/components/navigation/RightSidebar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
@@ -15,6 +13,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 mx-10 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
+        <RightSidebar />
       </div>
     </main>
   );
