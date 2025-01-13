47348e3c04ac6e30739808b1f54235d513d604c8
Aayushjain1407 <aayushjain1475@gmail.com>
2025-01-13 18:12:32 +0530

    Left Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,21 @@
 import { ReactNode } from "react";
 
 import Navbar from "@/components/navigation/navbar";
+import LeftSideBar from "@/components/navigation/LeftSidebar";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import { Link } from "lucide-react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main>
+    <main className="background-light850_dark100 relative">
       <Navbar />
-      {children}
+      <div className="flex">
+        <LeftSideBar />
+        <section className="flex min-h-screen flex-1 flex-col px-6 mx-10 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+      </div>
     </main>
   );
 };
