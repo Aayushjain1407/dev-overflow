a8567e2f01d8aa9c956dbfe4008aa53423f52d1a
Aayushjain1407 <aayushjain1475@gmail.com>
2025-01-13 14:38:38 +0530

    Created the UI for User Authentication

---
+++ lib/validation.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+export const SignInSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Email is required" })
+    .email({ message: "Please provide a valid email address." }),
+
+  password: z
+    .string()
+    .min(6, { message: "Password must be atleast 6 characters long. " })
+    .max(100, { message: "Password cannot exceed more than 100 characters. " }),
+});
+
+export const SignUpSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters long." })
+    .max(30, { message: "Username cannot exceed 30 characters." })
+    .regex(/^[a-zA-Z0-9_]+$/, {
+      message: "Username can only contain letters, numbers, and underscores.",
+    }),
+
+
+  });
