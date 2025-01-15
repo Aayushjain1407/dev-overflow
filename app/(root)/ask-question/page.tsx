e6e91eb51296818493393c4b574db0289a643bfa
Aayushjain1407 <aayushjain1475@gmail.com>
2025-01-15 12:31:31 +0530

    Implement the ui of QuestionForm

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,8 +1,17 @@
+import QuesitonForm from '@/components/forms/QuesitonForm'
 import React from 'react'
 
 const AskQuestion = () => {
   return (
-    <div>AskQuestion</div>
+    <>
+      <h1 className='h1-bold text-dark100_light900'>Ask A Question</h1>
+
+      <div className='mt-9'>
+        <QuesitonForm />
+      </div>
+   
+    </>
+
   )
 }
 
--- lib/validation.ts
+++ lib/validation.ts
@@ -1,3 +1,4 @@
+import { title } from "process";
 import { z } from "zod";
 
 export const SignInSchema = z.object({
@@ -20,6 +21,21 @@ export const SignUpSchema = z.object({
     .regex(/^[a-zA-Z0-9_]+$/, {
       message: "Username can only contain letters, numbers, and underscores.",
     }),
+});
 
-
-  });
+export const AskQuestionSchema = z.object({
+  title: z
+    .string()
+    .min(5, { message: "Title is required." })
+    .max(100, { message: "Title cannot exceed 100 characters." }),
+  content: z.string().min(1, { message: "Body is required." }),
+  tags: z
+    .array(
+      z
+        .string()
+        .min(1, { message: "Tag is required." })
+        .max(30, { message: "Tag cannot exceed 30 characters." })
+    )
+    .min(1, { message: "At least one tag is required." })
+    .max(3, { message: " Cannot add more than 3 tags." }),
+});
