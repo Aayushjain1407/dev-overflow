54b9b7a8f3ac458b80e25e82a5e93f9c7291d97a
Aayushjain1407 <aayushjain1475@gmail.com>
2025-01-20 13:51:36 +0530

    implement create question form logic

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,18 +1,21 @@
-import QuesitonForm from '@/components/forms/QuesitonForm'
-import React from 'react'
+import { auth } from "@/auth";
+import QuesitonForm from "@/components/forms/QuesitonForm";
+import ROUTES from "@/constants/routes";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const AskQuestion = () => {
+const AskQuestion = async () => {
+  const session = await auth();
+  if (!session) return redirect(ROUTES.SIGN_IN);
   return (
     <>
-      <h1 className='h1-bold text-dark100_light900'>Ask A Question</h1>
+      <h1 className="h1-bold text-dark100_light900">Ask A Question</h1>
 
-      <div className='mt-9'>
+      <div className="mt-9">
         <QuesitonForm />
       </div>
-   
     </>
+  );
+};
 
-  )
-}
-
-export default AskQuestion
+export default AskQuestion;
