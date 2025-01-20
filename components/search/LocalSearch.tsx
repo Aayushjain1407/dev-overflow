669a0efd4cd0bff04e1692509df27147f88e41fc
Aayushjain1407 <aayushjain1475@gmail.com>
2025-01-20 18:41:24 +0530

    display tags

--- components/search/LocalSearch.tsx
+++ components/search/LocalSearch.tsx
@@ -1,22 +1,33 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { Input } from "../ui/input";
+
 import Image from "next/image";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
 import { formUrlQuery, removeKeysFromUrlQuery } from "@/lib/url";
+
+import { Input } from "../ui/input";
 
 interface Props {
   route: string;
   imgSrc: string;
   placeholder: string;
   otherClasses?: string;
+  iconPosition?: "left" | "right";
 }
 
-const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
+const LocalSearch = ({
+  route,
+  imgSrc,
+  placeholder,
+  otherClasses,
+  iconPosition = "left",
+}: Props) => {
   const pathname = usePathname();
   const router = useRouter();
   const searchParams = useSearchParams();
   const query = searchParams.get("query") || "";
+
   const [searchQuery, setSearchQuery] = useState(query);
 
   useEffect(() => {
@@ -27,6 +38,7 @@ const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
           key: "query",
           value: searchQuery,
         });
+
         router.push(newUrl, { scroll: false });
       } else {
         if (pathname === route) {
@@ -34,33 +46,46 @@ const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
             params: searchParams.toString(),
             keysToRemove: ["query"],
           });
+
           router.push(newUrl, { scroll: false });
         }
       }
-    }, 1000);
+    }, 300);
+
     return () => clearTimeout(delayDebounceFn);
-  }, [searchQuery, router, route, searchParams]);
+  }, [searchQuery, router, route, searchParams, pathname]);
 
   return (
     <div
-      className={`background-light800_darkgradient flex min-h-[56px]
-     grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
+      className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
     >
-      <Image
-        src={imgSrc}
-        width={24}
-        height={24}
-        alt="SearchLogo"
-        className="cursor-pointer"
-      />
+      {iconPosition === "left" && (
+        <Image
+          src={imgSrc}
+          width={24}
+          height={24}
+          alt="Search"
+          className="cursor-pointer"
+        />
+      )}
+
       <Input
         type="text"
         placeholder={placeholder}
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
-        className="paragraph-regular no-focus placeholder 
-        text-dark-400_light700 border-none shadow-none outline-none"
+        className="paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
       />
+
+      {iconPosition === "right" && (
+        <Image
+          src={imgSrc}
+          width={15}
+          height={15}
+          alt="Search"
+          className="cursor-pointer"
+        />
+      )}
     </div>
   );
 };
